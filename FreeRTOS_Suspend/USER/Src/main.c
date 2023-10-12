c770b9e743252e5ab2c2cd86b283e9fe9e5176e5
36348 <363488120@qq.com>
2023-10-12 09:00:09 +0800

    智慧农业

--- FreeRTOS_Suspend/USER/Src/main.c
+++ FreeRTOS_Suspend/USER/Src/main.c
@@ -1,20 +1,22 @@
 #include "stm32f10x.h"
 #include "usart.h"
 #include "led.h"
+#include "key.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "delay.h"
 
 //任务优先级
 #define START_TASK_PRIO     1
 #define LED0_TASK_PRIO      2
 #define LED1_TASK_PRIO      3
-#define KEY_TASK_PRIO      4
+#define KEY_TASK_PRIO       4
 
 //任务堆栈大小
 #define START_STK_SIZE      128
 #define LED0_STK_SIZE       128
 #define LED1_STK_SIZE       128
-#define KEY_STK_SIZE       128
+#define KEY_STK_SIZE        128
 
 //任务句柄
 static TaskHandle_t AppTaskCreate_Handle;
@@ -22,7 +24,31 @@ static TaskHandle_t LED0_Task_Handle;
 static TaskHandle_t LED1_Task_Handle;
 static TaskHandle_t KEY_Task_Handle;
 
-static void led0_task(void *pvParameters)
+void key_task(void *pvParameters)
+{
+    u8 key_num;
+    while(1)
+    {
+        key_num = key_scan();
+        switch (key_num) {
+            case KEY0_P:
+                vTaskSuspend(LED0_Task_Handle);
+                printf("Task1 Suspend!!\r\n");
+                break;
+            case KEY1_P:
+                vTaskSuspend(LED1_Task_Handle);
+                printf("Task2 Suspend!!\r\n");
+                break;
+            case KEY2_P:
+                vTaskResume(LED0_Task_Handle);
+                vTaskResume(LED1_Task_Handle);
+                printf("Tasks Resume!!\r\n");
+        }
+        vTaskDelay(10);
+    }
+}
+
+void led0_task(void *pvParameters)
 {
     u32 task0_num = 0;
     while(1)
@@ -30,15 +56,15 @@ static void led0_task(void *pvParameters)
         task0_num++;
         LED0=!LED0;
         printf("Task1 Running: %lu!!\r\n", task0_num);
-        if (task0_num == 10){
-            vTaskDelete(LED1_Task_Handle);
-            printf("Task1 Delete Task2!!\r\n");
-        }
+//        if (task0_num == 10){
+//            vTaskDelete(LED1_Task_Handle);
+//            printf("Task1 Delete Task2!!\r\n");
+//        }
         vTaskDelay(500);
     }
 }
 
-static void led1_task(void *pvParameters)
+void led1_task(void *pvParameters)
 {
     u32 task1_num = 0;
     while(1)
@@ -77,9 +103,9 @@ void AppTaskCreate(void *pvParameters)
     //创建任务2
     xTaskCreate((TaskFunction_t )key_task,
                 (const char*    )"key_task",
-                (uint16_t       )LED1_STK_SIZE,
+                (uint16_t       )KEY_STK_SIZE,
                 (void*          )NULL,
-                (UBaseType_t    )LED1_TASK_PRIO,
+                (UBaseType_t    )KEY_TASK_PRIO,
                 (TaskHandle_t *) &KEY_Task_Handle);
     if (KEY_Task_Handle)
         printf("key_task任务创建成功！\r\n");
@@ -92,6 +118,8 @@ int main(void)
 {
     NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
     led_init();
+    key_init();
+    delay_init();
     uart_init(115200);
     PrintfInit(USART1);
     printf("正在启动.....\r\n");
